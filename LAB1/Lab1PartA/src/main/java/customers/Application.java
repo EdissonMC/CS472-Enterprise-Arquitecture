9f9ac2e0af4981cf9fcebe4208465ea3c71bc267
Angel M Correa <pattrick.adamss@gmail.com>
2023-07-24 17:10:05 -0500

    Solving Lab1: 3 WAYS TO CONFIGURE SPRING APPLICATIONS

---
+++ LAB1/Lab1PartA/src/main/java/customers/Application.java
@@ -0,0 +1,25 @@
+package customers;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Application {
+	public static void main(String[] args) {
+
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"springconfig.xml");
+
+		ICustomerService customerService = context.getBean("customerService",
+				ICustomerService.class);
+
+
+//			ICustomerDAO customerDAO ;//= new CustomerDAO();
+//			IEmailSender emailSender ;//= new EmailSender();
+
+
+
+		customerService.addCustomer("Frank Brown", "fbrown@acme.com",
+				"mainstreet 5", "Chicago", "60613");
+	}
+}
+
